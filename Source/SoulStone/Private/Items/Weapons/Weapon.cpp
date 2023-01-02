4f072fec217a41f5a86c61bd8bc39bb0c8117b83
Kareem Hegazy <kthegazy2002@gmail.com>
2023-01-01 21:02:02 -0500

    Added Weapon Hit Box

--- Source/SoulStone/Private/Items/Weapons/Weapon.cpp
+++ Source/SoulStone/Private/Items/Weapons/Weapon.cpp
@@ -5,6 +5,30 @@
 #include "Characters/SoulStoneCharacter.h"
 #include "Kismet/GameplayStatics.h"
 #include "Components/SphereComponent.h"
+#include "Components/BoxComponent.h"
+#include "Kismet/KismetSystemLibrary.h"
+
+AWeapon::AWeapon() 
+{
+	WeaponHitBox = CreateDefaultSubobject<UBoxComponent>(TEXT("Weapon HitBox"));
+	WeaponHitBox->SetupAttachment(GetRootComponent());
+	WeaponHitBox->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	WeaponHitBox->SetCollisionResponseToAllChannels(ECollisionResponse::ECR_Overlap);
+	WeaponHitBox->SetCollisionResponseToChannel(ECollisionChannel::ECC_Pawn, ECollisionResponse::ECR_Ignore);
+
+	HitBoxTraceStart = CreateDefaultSubobject<USceneComponent>(TEXT("Hit Box Trace Start"));
+	HitBoxTraceStart->SetupAttachment(GetRootComponent());
+
+	HitBoxTraceEnd = CreateDefaultSubobject<USceneComponent>(TEXT("Hit Box Trace End"));
+	HitBoxTraceEnd->SetupAttachment(GetRootComponent());
+}
+
+void AWeapon::BeginPlay() 
+{
+	Super::BeginPlay();
+
+	WeaponHitBox->OnComponentBeginOverlap.AddDynamic(this, &AWeapon::OnHitBoxOverlap);
+}
 
 void AWeapon::Equip(USceneComponent* InParent, FName InSocketName)
 {
@@ -36,4 +60,19 @@ void AWeapon::OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActo
 {
 	Super::OnSphereEndOverlap(OverlappedComponent, OtherActor, OtherComp, OtherBodyIndex);
 
+}
+
+void AWeapon::OnHitBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	const FVector Start = HitBoxTraceStart->GetComponentLocation();
+	const FVector End = HitBoxTraceEnd->GetComponentLocation();
+
+	TArray<AActor*> ActorsToIgnore;
+	ActorsToIgnore.Add(this);
+
+	FHitResult TraceHits;
+
+	UKismetSystemLibrary::BoxTraceSingle(this, Start, End, FVector(5.f, 5.f, 5.f),
+		HitBoxTraceStart->GetComponentRotation(), ETraceTypeQuery::TraceTypeQuery1, 
+		false, ActorsToIgnore, EDrawDebugTrace::ForDuration, TraceHits, true);
 }
--- Source/SoulStone/Public/Items/Weapons/Weapon.h
+++ Source/SoulStone/Public/Items/Weapons/Weapon.h
@@ -7,24 +7,38 @@
 #include "Weapon.generated.h"
 
 class USoundBase;
-/**
- * 
- */
+class UBoxComponent;
+
 UCLASS()
 class SOULSTONE_API AWeapon : public AItem
 {
 	GENERATED_BODY()
 
 public:
-	void Equip(USceneComponent* InParent, FName InSocketName);
 
+	AWeapon();
+	void Equip(USceneComponent* InParent, FName InSocketName);
 	void AttachMeshToSocket(USceneComponent* InParent, const FName& InSocketName);
 
 protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
 	virtual void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult) override;
 	virtual void OnSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex) override;
+
+	UFUNCTION()
+	void OnHitBoxOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 
 private:
 	UPROPERTY(EditAnywhere, Category = "WeaponProperties")
 	USoundBase* EquipSound;
+
+	UPROPERTY(VisibleAnywhere, Category = "WeaponProperties")
+	UBoxComponent* WeaponHitBox;
+
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* HitBoxTraceStart;
+	UPROPERTY(VisibleAnywhere)
+	USceneComponent* HitBoxTraceEnd;
 };
