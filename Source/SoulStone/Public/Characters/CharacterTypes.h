4b4a30e2c5c17f0826f203db8930bf999b319cda
Kareem Hegazy <kthegazy2002@gmail.com>
2023-01-08 00:53:59 -0500

    Improved Warp Targets

--- Source/SoulStone/Public/Characters/CharacterTypes.h
+++ Source/SoulStone/Public/Characters/CharacterTypes.h
@@ -12,7 +12,7 @@ enum class EActionState : uint8 {
 	EAS_Unoccupied UMETA(DisplayName = "Unoccupied"),
 	EAS_HitReaction UMETA(DisplayName = "HitReaction"),
 	EAS_Attacking UMETA(DisplayName = "Attacking"),
-	EAS_EquippingWeapon UMETA(DisplayName = "Equipping Weapon"),
+	EAS_EquippingWeapon UMETA(DisplayName = "Equipping Weapon")
 };
 
 UENUM(BlueprintType)
@@ -29,10 +29,10 @@ enum EDeathPose {
 
 UENUM(BlueprintType)
 enum class EEnemyState : uint8 {
+	EES_NoState UMETA(DisplayName = "NoState"),
 	EES_Dead UMETA(DisplayName = "Dead"),
 	EES_Patrolling UMETA(DisplayName = "Patrolling"),
 	EES_Chasing UMETA(DisplayName = "Chasing"),
 	EES_Attacking UMETA(DisplayName = "Attacking"),
-	EES_Engaged UMETA(DisplayName = "Engaged"),
-	EES_NoState UMETA(DisplayName = "NoState"),
+	EES_Engaged UMETA(DisplayName = "Engaged")
 };
--- Source/SoulStone/Public/Enemy/Enemy.h
+++ Source/SoulStone/Public/Enemy/Enemy.h
@@ -49,9 +49,6 @@ protected:
 
 	UPROPERTY(BlueprintReadOnly)
 	EEnemyState EnemyState = EEnemyState::EES_Patrolling;
-
-	UPROPERTY(BlueprintReadOnly, Category = Combat)
-	AActor* CombatTarget;
 private:
 	/** AI Behavior */
 	void InitializeEnemy();
